dd111dbb5f39d7329365ab210935b50b3bbd6888
Blacky <97872836+DrBlack22@users.noreply.github.com>
2023-11-13 20:16:46 +0500

    First commit

---
+++ Inheritance.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Mother.h"
+#include "Mother.cpp"
+#include "Daughter.h"
+#include "Daughter.cpp"
+using namespace std;
+int main(){
+
+    Daughter maxy;
+    maxy.sayName();
+}
+
+//Mother is base class
+//Daughter is derived class
---
+++ Mother.cpp
@@ -0,0 +1,11 @@
+#include "Mother.h"
+#include <iostream>
+using namespace std;
+
+Mother::Mother()
+{
+
+}
+void Mother::sayName(){
+    cout << "I AM UR MOM!!" << endl;
+}
